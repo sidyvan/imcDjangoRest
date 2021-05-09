780ee7dbd32f1334cefd5920eed785b2e11ff51a
Sidyvan Fernandes de Andrade <sidyvan.dev@gmail.com>
2021-05-09 08:39:21 -0300

    API Calcula IMC

--- imc/serializers.py
+++ imc/serializers.py
@@ -5,5 +5,11 @@ class ImcSerializer(serializers.Serializer):
 
     peso = serializers.FloatField()
     altura = serializers.FloatField()
-    
+    imc = serializers.SerializerMethodField()
 
+    def get_imc(self, obj):
+        peso = obj['peso']
+        altura = obj['altura']
+        imc = peso / (altura * altura)
+
+        return imc
