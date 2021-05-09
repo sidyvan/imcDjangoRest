ef03f07482eae8022289cf857b3f735ed473f834
Sidyvan Fernandes de Andrade <sidyvan.dev@gmail.com>
2021-05-09 08:53:06 -0300

    Calculando IMC

--- imc/serializers.py
+++ imc/serializers.py
@@ -8,8 +8,26 @@ class ImcSerializer(serializers.Serializer):
     imc = serializers.SerializerMethodField()
 
     def get_imc(self, obj):
+        resposta = None
+
         peso = obj['peso']
         altura = obj['altura']
         imc = peso / (altura * altura)
 
-        return imc
+        if(imc < 18.5):
+            resposta = "MAGREZA"
+        elif(18.5 <= imc <= 24.9):
+            resposta = "NORMAL"
+        elif(25.0 <= imc <= 29.9):
+            resposta = "SOBREPESO"
+
+        elif(30.0 <= imc <= 39.9):
+            resposta = "OBESIDADE"
+
+        elif(40.0 < imc):
+            resposta = "OBESIDADE GRAVE"
+
+
+
+
+        return resposta
